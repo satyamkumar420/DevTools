e58bf2f137932b5af4712485e08ef9210b982d50
satyamkumar420 <satyamkumar2460@gmail.com>
2023-10-16 16:38:48 +0530

    changes

--- src/Components/utils/Button/Button.jsx
+++ src/Components/utils/Button/Button.jsx
@@ -1,6 +1,19 @@
+import React, { useState } from "react";
+
 const Button = ({ text, icon, onClick, className }) => {
+  const [hovered, setHovered] = useState(false);
+
   const getRandomGradient = () => {
-    const colors = ["#f5d939", "#ff0099", "#0000FF", "#00ff91", "#00FFFF"];
+    const colors = [
+      "#f5d939",
+      "#ff0099",
+      "#0000FF",
+      "#00ff91",
+      "#00FFFF",
+      "#9b730a",
+      "#702fd0",
+      "#986278",
+    ];
     const randomIndex = Math.floor(Math.random() * colors.length);
     const color1 = colors[randomIndex];
     const color2 = colors[(randomIndex + 1) % colors.length];
@@ -8,14 +21,35 @@ const Button = ({ text, icon, onClick, className }) => {
   };
 
   const getRandomColor = () => {
-    const colors = ["#f5d939", "#ff0099", "#0000FF", "#00ff91", "#00FFFF"];
-    const randomIndex = Math.floor(Math.random() * colors.length);
+    const colors = [
+      "#ff0099",
+      "#961541",
+      "#613c47",
+      "#0000FF",
+      "#1e3c2c",
+      "#fe8519",
+      "#2f2d39",
+    ];
+    let randomIndex;
+
+    do {
+      randomIndex = Math.floor(Math.random() * colors.length);
+    } while (randomIndex === getRandomColor.lastRandomIndex);
+
+    getRandomColor.lastRandomIndex = randomIndex;
     return colors[randomIndex];
   };
 
+  getRandomColor.lastRandomIndex = -1;
+
+  // hover gradient
+  const bgGradient = `linear-gradient(#161725, #161725) padding-box,
+               ${getRandomGradient()} border-box`;
+
+  const hoverBgGradient = `linear-gradient(60deg, ${getRandomColor()}, ${getRandomColor()}) padding-box, ${getRandomGradient()} border-box`;
+
   const buttonStyle = {
-    background: `linear-gradient(#161725, #161725) padding-box,
-               ${getRandomGradient()} border-box`,
+    background: `${hovered ? hoverBgGradient : bgGradient}`,
     boxShadow:
       "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     color: "#e9e9ff",
@@ -25,6 +59,15 @@ const Button = ({ text, icon, onClick, className }) => {
     display: "inline-block",
     fontSize: "22px",
     cursor: "pointer",
+    transition: "background 0.3s ease",
+  };
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
   };
 
   return (
@@ -33,6 +76,8 @@ const Button = ({ text, icon, onClick, className }) => {
       onClick={onClick}
       style={buttonStyle}
       type="button"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div className={`${className}`}>
         {icon && <span className="md:text-3xl">{icon}</span>}
