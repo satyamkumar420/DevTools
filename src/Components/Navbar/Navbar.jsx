3d071d4eaa814d355c2272b1bc796e4376b95b6d
satyamkumar420 <satyamkumar2460@gmail.com>
2023-11-07 14:42:57 +0530

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,68 +1,66 @@
-import Sidebar from "./Sidebar";
-import { NavLink } from "react-router-dom";
-import { IconLanguageHiragana } from "../Icons/Icons";
-import { IconMenuLeft } from "../Icons/Icons";
-import { useState } from "react";
-import Logo from "../../assets/favicon.png";
+import Sidebar from './Sidebar';
+import { NavLink } from 'react-router-dom';
+import { IconLanguageHiragana } from '../Icons/Icons';
+import { IconMenuLeft } from '../Icons/Icons';
+import { useState } from 'react';
+import Logo from '../../assets/favicon.png';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <>
-      <header className="fixed w-full top-0 left-0 right-0 z-50  backdrop-blur-3xl firefox:bg-opacity-50 p-4  flex shadow-lg  justify-between items-center">
-        <div className="ml-3 flex">
-          <div className="sm:hidden  p-1" onClick={toggleMenu}>
-            <IconMenuLeft className="font-bold text-4xl cursor-pointer" />
-          </div>
+	return (
+		<>
+			<header className="fixed w-full top-0 left-0 right-0 z-50  backdrop-blur-3xl firefox:bg-opacity-50 p-4  flex shadow-lg  justify-between items-center">
+				<div className="ml-3 flex">
+					<div className="sm:hidden  p-1" onClick={toggleMenu}>
+						<IconMenuLeft className="font-bold text-4xl cursor-pointer" />
+					</div>
 
-          <NavLink
-            to="/"
-            className="text-blue-50 text-xl font-bold cursor-pointer "
-          >
-            <div className="flex items-center">
-              <img
-                src={Logo}
-                alt="Logo"
-                width="40px"
-                height="auto"
-                className="mr-2"
-              />
-              <div className="hidden sm:block">
-                <span className="text-pink-500 underline">Dev</span>
-                <span className="text-blue-50">Tools</span>
-              </div>
-            </div>
-          </NavLink>
-        </div>
-        <div className="flex   items-center  border-[#1a1c2e] p-1 rounded bg-[#1a1c2e] border-2 mr-6">
-          <IconLanguageHiragana />
-          <select
-            className="bg-[#1a1c2e] text-base text-blue-50  border-[#1a1c2e] p-2 rounded px-3 cursor-pointer outline-none"
-            //   onChange={(e) => console.log(e.target.value)} // Handle language change here
-          >
-            <option value="en">English</option>
-            {/* <option value="es">Español</option>
+					<NavLink
+						to="/"
+						className="text-blue-50 text-xl font-bold cursor-pointer ">
+						<div className="flex items-center">
+							<img
+								src={Logo}
+								alt="Logo"
+								width="40"
+								height="40"
+								className="mr-2"
+							/>
+							<div className="hidden sm:block">
+								<span className="text-pink-500 underline">Dev</span>
+								<span className="text-blue-50">Tools</span>
+							</div>
+						</div>
+					</NavLink>
+				</div>
+				<div className="flex   items-center  border-[#1a1c2e] p-1 rounded bg-[#1a1c2e] border-2 mr-6">
+					<IconLanguageHiragana />
+					<select
+						className="bg-[#1a1c2e] text-base text-blue-50  border-[#1a1c2e] p-2 rounded px-3 cursor-pointer outline-none"
+						//   onChange={(e) => console.log(e.target.value)} // Handle language change here
+					>
+						<option value="en">English</option>
+						{/* <option value="es">Español</option>
             <option value="fr">Français</option> */}
-          </select>
-        </div>
-      </header>
+					</select>
+				</div>
+			</header>
 
-      <div
-        className={`z-50 transition-all duration-100 ease-in-out sm:block transform ${
-          isOpen
-            ? "translate-x-0 z-[999999] relative opacity-100 "
-            : "-translate-x-full sm:translate-x-0"
-        }`}
-      >
-        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-      </div>
-    </>
-  );
+			<div
+				className={`z-50 transition-all duration-100 ease-in-out sm:block transform ${
+					isOpen
+						? 'translate-x-0 z-[999999] relative opacity-100 '
+						: '-translate-x-full sm:translate-x-0'
+				}`}>
+				<Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+			</div>
+		</>
+	);
 };
 
 export default Navbar;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,16 +10,16 @@ const Home = () => {
 			<Helmet>
 				<title>Home | DevTools</title>
 			</Helmet>
-			<div className='p-4 sm:ml-52 max-w-screen  overflow-y-auto max-h-screen'>
-				<div className='max-w-screen-lg p-4 border-gray-200 rounded-lg mt-20 mb-20'>
-					<h3 className='p-2 rounded sm:text-2xl text-yellow-500 w-full bg-[#1a1c2e]'>
+			<div className="p-4 sm:ml-52 max-w-screen  overflow-y-auto max-h-screen">
+				<div className="max-w-screen-lg p-4 border-gray-200 rounded-lg mt-20 mb-20">
+					<h3 className="p-2 rounded sm:text-2xl text-yellow-500 w-full bg-[#1a1c2e]">
 						Home
 					</h3>
-					<div className='text-left'>
-						<h1 className='font-extrabold text-transparent sm:text-4xl bg-clip-text bg-gradient-to-r from-pink-400 to-red-700 text-xl mt-3'>
+					<div className="text-left">
+						<h1 className="font-extrabold text-transparent sm:text-4xl bg-clip-text bg-gradient-to-r from-pink-400 to-red-700 text-xl mt-3">
 							Welcome to Online DevTools!
 						</h1>
-						<p className='text-blue-50 mt-3 leading-normal text-sm sm:text-lg'>
+						<p className="text-white mt-3 leading-normal text-sm sm:text-lg">
 							Our platform is here to make your life easier! We provide a wide
 							variety of free online resources that are tailored to meet your
 							specific needs. With our platform, you'll experience unparalleled
@@ -27,34 +27,27 @@ const Home = () => {
 							activities.
 						</p>
 					</div>
-					<div className='mt-5'>
-						<div className=' my-5 text-green-400 font-semibold text-sm sm:text-xl flex gap-2 items-center'>
-							<IconShieldTick className='text-sm sm:text-xl' />
+					<div className="mt-5">
+						<div className=" my-5 text-green-400 font-semibold text-sm sm:text-xl flex gap-2 items-center">
+							<IconShieldTick className="text-sm sm:text-xl" />
 							<h2>Online tools available for you:</h2>
 						</div>
-						<div className=' flex flex-wrap sm:gap-3 gap-2'>
+						<div className=" flex flex-wrap sm:gap-3 gap-2">
 							{HomeLinks.map((button, index) => (
-								<NavLink to={button.link} key={index} className=''>
+								<NavLink to={button.link} key={index} className="">
 									<Button
 										text={button.text}
-										className='text-lg md:text-xl p-2 sm:p-2 px-2'
+										className="text-lg md:text-xl p-2 sm:p-2 px-2"
 									/>
 								</NavLink>
 							))}
 						</div>
 					</div>
-					{/* <div className="mt-10">
-            <div className="border-l-4 border-l-purple-500 p-2 text-sm sm:text-lg bg-[#1a1c2e]">
-              <strong className="text-orange-500 text-center">Note</strong>{" "}
-              <span className="text-blue-300">
-                :- This website is still under development!
-              </span>
-            </div>
-          </div> */}
-					<div className='mt-4'>
-						<div className='border-l-4 border-l-purple-500 p-2 text-sm sm:text-lg bg-[#1a1c2e]'>
-							<strong className='text-orange-500 text-center'>Note</strong>{' '}
-							<span className='text-blue-300'>
+
+					<div className="mt-4">
+						<div className="border-l-4 border-l-purple-500 p-2 text-sm sm:text-lg bg-[#1a1c2e]">
+							<strong className="text-orange-500 text-center">Note</strong>{' '}
+							<span className="text-blue-300">
 								:- I've developed a website called DevTools that offers a
 								variety of tools to improve web development and design
 								processes. Some of these key features include image cropping,
