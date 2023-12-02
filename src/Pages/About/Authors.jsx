efc56456d9aca7460feebde297a885b8da943ba7
satyamkumar420 <satyamkumar2460@gmail.com>
2023-12-02 22:12:13 +0530

    changes

--- src/Pages/About/Authors.jsx
+++ src/Pages/About/Authors.jsx
@@ -21,49 +21,53 @@ const Authors = () => {
       <div className="p-4 sm:ml-52 text-justify max-w-screen overflow-y-auto max-h-screen">
         <div className="my-20 max-w-screen-lg">
           <h3 className="p-2 rounded text-lg sm:text-2xl text-yellow-500 w-full bg-[#1a1c2e]">
-            Authers Details
+            Authors Details
           </h3>
-          <div className="text-left mt-5 bg-[#1a1c2e] p-5 rounded-lg text-sm sm:text-lg w-fit shadow-lg">
-            <div className="flex justify-center">
-              <img
-                src={AuthorImage}
-                className="rounded-full"
-                alt="Authors"
-                width={200}
-                height={200}
-              />
-            </div>
-            <div className="text-center">
-              <h1 className="text-4xl text-white font-medium my-2">
-                Satyam Kumar
-              </h1>
-              <h3 className="mb-2 text-xl">🥷Full Stack Developer🥷</h3>
-              <h3 className="text-gray-400 mb-3">R.G.P.V University, Bhopal</h3>
-              <div className="flex justify-center gap-x-3 text-2xl">
-                <NavLink
-                  className="text-white hover:text-blue hover:bg-blue-500/10 rounded-full p-1 "
-                  to="https://github.com/satyamkumar420"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <IconGithub />
-                </NavLink>
-                <NavLink
-                  className="text-white hover:text-blue hover:bg-blue-400/10  rounded-full p-1"
-                  to="https://www.linkedin.com/in/satyamkumar404/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <IconLinkedinCircled />
-                </NavLink>
-                <NavLink
-                  className="text-white hover:text-blue hover:bg-blue-400/10  rounded-full p-1"
-                  to="mailto:satyamkumar2460@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <IconEmailOpen />
-                </NavLink>
+          <div className="flex justify-center md:justify-start">
+            <div className=" mt-5 bg-[#1a1c2e] p-5 rounded-lg text-sm sm:text-lg w-fit shadow-lg">
+              <div className="flex justify-center">
+                <img
+                  src={AuthorImage}
+                  className="rounded-full"
+                  alt="Authors"
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-4xl text-white font-medium my-2">
+                  Satyam Kumar
+                </h1>
+                <h3 className="mb-2 text-xl">🥷Full Stack Developer🥷</h3>
+                <h3 className="text-gray-400 mb-3">
+                  R.G.P.V University, Bhopal
+                </h3>
+                <div className="flex justify-center gap-x-3 text-2xl">
+                  <NavLink
+                    className="text-white hover:text-blue hover:bg-blue-500/10 rounded-full p-1 "
+                    to="https://github.com/satyamkumar420"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IconGithub />
+                  </NavLink>
+                  <NavLink
+                    className="text-white hover:text-blue hover:bg-blue-400/10  rounded-full p-1"
+                    to="https://www.linkedin.com/in/satyamkumar404/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IconLinkedinCircled />
+                  </NavLink>
+                  <NavLink
+                    className="text-white hover:text-blue hover:bg-blue-400/10  rounded-full p-1"
+                    to="mailto:satyamkumar2460@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <IconEmailOpen />
+                  </NavLink>
+                </div>
               </div>
             </div>
           </div>
