6f84afe1cb8af5a605a78c1b0baf67df34ab8017
satyamkumar420 <satyamkumar2460@gmail.com>
2024-01-04 17:31:43 +0530

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="fixed bottom-0 left-0 right-0 bg-[#1a1c2e] text-white py-3 px-5  sm:ml-[185px] max-w-screen-2xl ">
       <div className="grid sm:flex sm:flex-wrap gap-4 justify-center md:justify-end mx-auto  text-sm lg:text-base text-gray-400">
         <p className="text-left sm:text-center flex">
-          &copy; 2023
+          &copy; 2024
           <strong className="text-pink-500 mx-1 animate-ping">
             <IconHeart />
           </strong>
